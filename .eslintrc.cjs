b65dbaf2195beaa2927636f99fe44c7c561ef47b
Edward Chen <edwardchen@CXZ-STS-01-37.local>
2025-10-02 13:43:03 +1000

    convert to TS

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,17 @@
 module.exports = {
-  env: { browser: true, es2023: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended'
-  ],
+  root: true,
+  env: { browser: true, es2023: true, node: true },
+  parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: 'detect' } },
-  plugins: ['react', 'react-hooks', 'import'],
+  plugins: ['react-refresh', 'react', '@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
   rules: {
-    'react/react-in-jsx-scope': 'off'
+    'react/react-in-jsx-scope': 'off',
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
   }
 };
